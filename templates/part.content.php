2dfbccf58409a409996e166df0dfa6860a1d0934
Ivan Sein <ivan@struktur.de>
2016-08-03 11:25:48 +0200

    remove empty space from translatable strings

--- templates/part.content.php
+++ templates/part.content.php
@@ -10,30 +10,30 @@
 </div>
 <div class="section" id="storageSection">
 	<h2><?php p($l->t('Storage'));?></h2>
-	<p><?php p($l->t('Users: '));?><em id="numUsersStorage">--</em></p>
-	<p><?php p($l->t('Files: '));?><em id="numFilesStorage">--</em></p>
-	<p><?php p($l->t('Storages: '));?></p>
+	<p><?php p($l->t('Users:'));?><em id="numUsersStorage"> -- </em></p>
+	<p><?php p($l->t('Files:'));?><em id="numFilesStorage"> -- </em></p>
+	<p><?php p($l->t('Storages:'));?></p>
 	<div id="container" style="width:280px; height:250px;">
 		<canvas id="storagescanvas" width="280" height="250"></canvas>
 	</div>
 </div>
 <div class="section" id="shareSection">
 	<h2><?php p($l->t('Shares'));?></h2>
-	<p><?php p($l->t('Total: '));?></p>
+	<p><?php p($l->t('Total:'));?><em id="totalShares"> -- </em></p>
 	<div id="container" style="width:350px; height:350px;">
 		<canvas id="sharecanvas" width="350" height="350"></canvas>
 	</div>
 </div>
 <div class="section" id="phpSection">
 	<h2><?php p($l->t('PHP'));?></h2>
-	<p><?php p($l->t('Version: '));?><em id="phpVersion">--</em></p>
-	<p><?php p($l->t('Memory Limit: '));?><em id="phpMemLimit">--</em></p>
-	<p><?php p($l->t('Max Execution Time: '));?><em id="phpMaxExecTime">--</em></p>
-	<p><?php p($l->t('Upload max size: '));?><em id="phpUploadMaxSize">--</em></p>
+	<p><?php p($l->t('Version:'));?><em id="phpVersion"> -- </em></p>
+	<p><?php p($l->t('Memory Limit:'));?><em id="phpMemLimit"> -- </em></p>
+	<p><?php p($l->t('Max Execution Time:'));?><em id="phpMaxExecTime"> -- </em></p>
+	<p><?php p($l->t('Upload max size:'));?><em id="phpUploadMaxSize"> -- </em></p>
 </div>
 <div class="section" id="databaseSection">
 	<h2><?php p($l->t('Database'));?></h2>
-	<p><?php p($l->t('Type: '));?><em id="databaseType">--</em></p>
-	<p><?php p($l->t('Version: '));?><em id="databaseVersion">--</em></p>
-	<p><?php p($l->t('Size: '));?><em id="dataBaseSize">--</em></p>
+	<p><?php p($l->t('Type:'));?><em id="databaseType"> -- </em></p>
+	<p><?php p($l->t('Version:'));?><em id="databaseVersion"> -- </em></p>
+	<p><?php p($l->t('Size:'));?><em id="dataBaseSize"> -- </em></p>
 </div>
