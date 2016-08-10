a9f2ea2088ef4f2339e020a8aebf12ab80a74b56
Bjoern Schiessle <bjoern@schiessle.org>
2016-08-10 13:55:26 +0200

    move server info to the admin page

--- appinfo/app.php
+++ appinfo/app.php
@@ -23,36 +23,4 @@ namespace OCA\ServerInfo\AppInfo;
 
 use OCP\AppFramework\App;
 
-$app = new App('serverinfo');
-$container = $app->getContainer();
-
-$groupManager = \OC::$server->getGroupManager();
-$user = \OC::$server->getUserSession()->getUser();
-$isAdmin = $user !== null && $groupManager->isAdmin($user->getUID());
-
-
-if ($isAdmin) {
-	$container->query('OCP\INavigationManager')->add(function () use ($container) {
-		$urlGenerator = $container->query('OCP\IURLGenerator');
-		$l10n = $container->query('OCP\IL10N');
-		return [
-			// the string under which your app will be referenced in owncloud
-			'id' => 'serverinfo',
-
-			// sorting weight for the navigation. The higher the number, the higher
-			// will it be listed in the navigation
-			'order' => 10,
-
-			// the route that will be shown on startup
-			'href' => $urlGenerator->linkToRoute('serverinfo.page.index'),
-
-			// the icon that will be shown in the navigation
-			// this file needs to exist in img/
-			'icon' => $urlGenerator->imagePath('serverinfo', 'app.svg'),
-
-			// the title of your application. This will be used in the
-			// navigation or on the settings page of your app
-			'name' => $l10n->t('Server Info'),
-		];
-	});
-}
+new App('serverinfo');
