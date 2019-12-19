114a7e896851b2adb1c77b81c6f2f5e19b6fda5f
Kevin Pfeifer <kevin.pfeifer@sunlime.at>
2019-12-19 20:53:52 +0100

    fix code indention + convert snake case variabs to camelCase
    Signed-off-by: Kevin Pfeifer <kevin.pfeifer@sunlime.at>

--- templates/settings-admin.php
+++ templates/settings-admin.php
@@ -1,40 +1,40 @@
 <?php
-	/**
-	 * @copyright Copyright (c) 2016 Bjoern Schiessle <bjoern@schiessle.org>
-	 *
-	 * @license GNU AGPL version 3 or any later version
-	 *
-	 * This program is free software: you can redistribute it and/or modify
-	 * it under the terms of the GNU Affero General Public License as
-	 * published by the Free Software Foundation, either version 3 of the
-	 * License, or (at your option) any later version.
-	 *
-	 * This program is distributed in the hope that it will be useful,
-	 * but WITHOUT ANY WARRANTY; without even the implied warranty of
-	 * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	 * GNU Affero General Public License for more details.
-	 *
-	 * You should have received a copy of the GNU Affero General Public License
-	 * along with this program.  If not, see <http://www.gnu.org/licenses/>.
-	 *
-	 */
+/**
+ * @copyright Copyright (c) 2016 Bjoern Schiessle <bjoern@schiessle.org>
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 
-	script('serverinfo', 'script');
-	script('serverinfo', 'smoothie');
-	script('serverinfo', 'Chart.min');
+script('serverinfo', 'script');
+script('serverinfo', 'smoothie');
+script('serverinfo', 'Chart.min');
 
-	style('serverinfo', 'style');
+style('serverinfo', 'style');
 
-	function FormatBytes($byte) {
-		$unim = array('B', 'KB', 'MB', 'GB', 'TB', 'PB');
-		$count = 1;
-		while ($byte >= 1024) {
-			$count++;
-			$byte = $byte / 1024;
-		}
+function FormatBytes($byte) {
+    $unim = array('B', 'KB', 'MB', 'GB', 'TB', 'PB');
+    $count = 1;
+    while ($byte >= 1024) {
+        $count++;
+        $byte = $byte / 1024;
+    }
 
-		return number_format($byte, 2, '.', '.') . ' ' . $unim[$count];
-	}
+    return number_format($byte, 2, '.', '.') . ' ' . $unim[$count];
+}
 
 ?>
 
