84f68b606ae19c36317dcd2ac6be079dc0f825fe
Matthew Wener <42217920+constrict@users.noreply.github.com>
2020-06-07 11:44:31 -0400

    Extend ServerInfo support to FreeBSD

--- lib/OperatingSystems/FreeBSD.php
+++ lib/OperatingSystems/FreeBSD.php
@@ -20,6 +20,8 @@
 
 namespace OCA\ServerInfo\OperatingSystems;
 
+use bantu\IniGetWrapper\IniGetWrapper;
+
 /**
  * Class FreeBSD
  *
@@ -130,8 +132,8 @@ class FreeBSD {
 			if ($iface['interface'] !== 'lo0') {
 				$iface['status'] = shell_exec('/sbin/ifconfig ' . $iface['interface'] . ' | grep "status" | cut -f2 -d$\'\t\' | cut -f2 -d \' \'');
 				$iface['speed']  = shell_exec('/sbin/ifconfig ' . $iface['interface'] . ' | grep "media" | cut -d \' \' -f3 | cut -f1 -d \'b\'');
-					if ($iface['speed'] !== '') {
-				$iface['speed'] = $iface['speed'];
+				if ($iface['speed'] !== '') {
+					$iface['speed'] = $iface['speed'];
 				} else {
 					$iface['speed'] = 'unknown';
 				}
@@ -149,7 +151,7 @@ class FreeBSD {
 			}
 			$result[] = $iface;
 		}
-
+		
 		return $result;
 	}
 
