339ffd87ef3888f85757140d4db80db5682b61df
Starkstromkonsument <it@starkstromkonsument.de>
2020-06-25 21:27:24 +0200

    Add status information about the opcache and APCu instances (#212)
    * Add status information about the opcache instance (Zend opcache)
    
    This commit adds the new subarray $ocs.data.server.php.opcache
    
    'N/A' will be returned, if no data can be retrieved.
    
    Further reference:
    
    - https://www.php.net/manual/en/function.opcache-get-status.php
    
    Signed-off-by: Starkstromkonsument <it@starkstromkonsument.de>
    
    * Add status information about the APCu cache instance
    
    This commit adds the new subarray $ocs.data.server.php.apcu
    
    'N/A' will be returned, if no data can be retrieved.
    
    Further reference:
    
    - https://www.php.net/manual/en/function.apcu-cache-info.php
    - https://www.php.net/manual/en/function.apcu-sma-info.php
    
    Signed-off-by: Starkstromkonsument <it@starkstromkonsument.de>
    
    * php-cs check fixes for opcache / APCu stats
    
    Signed-off-by: Starkstromkonsument <it@starkstromkonsument.de>
    
    * [opcache/APUc] fix names of variables
    
    as suggested by kesselb
    
    Co-authored-by: kesselb <mail@danielkesselberg.de>
    Signed-off-by: Starkstromkonsument <it@starkstromkonsument.de>
    
    * [opcache/APUc] return empty arrays instead of 'N/A'
    
    Co-authored-by: kesselb <mail@danielkesselberg.de>
    Signed-off-by: Starkstromkonsument <it@starkstromkonsument.de>
    
    * [opcache/APUc] code optimizations
    
    Co-authored-by: kesselb <mail@danielkesselberg.de>
    Signed-off-by: Starkstromkonsument <it@starkstromkonsument.de>
    
    Co-authored-by: kesselb <mail@danielkesselberg.de>

--- lib/PhpStatistics.php
+++ lib/PhpStatistics.php
@@ -47,6 +47,8 @@ class PhpStatistics {
 			'memory_limit' => $this->phpIni->getBytes('memory_limit'),
 			'max_execution_time' => $this->phpIni->getNumeric('max_execution_time'),
 			'upload_max_filesize' => $this->phpIni->getBytes('upload_max_filesize'),
+			'opcache' => $this->getOPcacheStatus(),
+			'apcu' => $this->getAPCuStatus(),
 		];
 	}
 
@@ -63,5 +65,63 @@ class PhpStatistics {
 			return $matches[0];
 		}
 		return $version;
+	}
+
+	/**
+	 * Get status information about the cache from the OPcache extension
+	 *
+	 * @return array with an array of state information about the cache instance
+	 */
+	protected function getOPcacheStatus(): array {
+		// Test if the OPcache module is installed
+		if (!extension_loaded('Zend OPcache')) {
+			// module not loaded, returning back empty array to prevent any errors on JS side.
+			return [];
+		}
+
+		// get status information about the cache
+		$status = opcache_get_status(false);
+
+		if ($status === false) {
+			// no array, returning back empty array to prevent any errors on JS side.
+			$status = [];
+		}
+
+		return $status;
+	}
+
+	/**
+	 * Get status information about the cache from the APCu extension
+	 *
+	 * @return array with an array of state information about the cache instance
+	 */
+	protected function getAPCuStatus(): array {
+		// Test if the APCu module is installed
+		if (!extension_loaded('apcu')) {
+			// module not loaded, returning back empty array to prevent any errors on JS side.
+			return [];
+		}
+
+		// get cached information from APCu data store
+		$cacheInfo = apcu_cache_info(true);
+
+		// get APCu Shared Memory Allocation information
+		$smaInfo = apcu_sma_info(true);
+
+		if ($cacheInfo === false) {
+			// no array, returning back N/A to prevent any errors on JS side.
+			$cacheInfo = 'N/A';
+		}
+
+		if ($smaInfo === false) {
+			// no array, returning back N/A to prevent any errors on JS side.
+			$smaInfo = 'N/A';
+		}
+
+		// return the array
+		return [
+			'cache' => $cacheInfo,
+			'sma' => $smaInfo,
+		];
 	}
 }
