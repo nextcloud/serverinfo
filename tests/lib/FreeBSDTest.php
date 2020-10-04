49952d29bb24c6592ba1ec820b930d007f6bb6c5
Matthew Wener <42217920+constrict@users.noreply.github.com>
2020-10-04 02:34:28 -0400

    Fix Memory (#240)
    * Fix Memory
    
    Cast swapFree to integer to fix typeerror.
    
    Fix total memory to use hw.realmem
    
    Signed-off-by: Matthew Wener <matthew@wener.org>
    Signed-off-by: Matthew Wener <mwener115@gmail.com>
    
    * Prevent float in swapinfo and update tests for hw.realmem
    
    Signed-off-by: Matthew Wener <matthew@wener.org>
    Signed-off-by: Matthew Wener <mwener115@gmail.com>
    
    * Update testGetMemoryNoSwapinfo to hw.realmem
    
    Signed-off-by: Matthew Wener <mwener115@gmail.com>

--- tests/lib/FreeBSDTest.php
+++ tests/lib/FreeBSDTest.php
@@ -54,7 +54,7 @@ class FreeBSDTest extends TestCase {
 		$this->os->method('executeCommand')
 			->willReturnMap([
 				['/usr/sbin/swapinfo -k', file_get_contents(__DIR__ . '/../data/freebsd_swapinfo')],
-				['/sbin/sysctl -n hw.physmem hw.pagesize vm.stats.vm.v_inactive_count vm.stats.vm.v_cache_count vm.stats.vm.v_free_count', file_get_contents(__DIR__ . '/../data/freebsd_meminfo')],
+				['/sbin/sysctl -n hw.realmem hw.pagesize vm.stats.vm.v_inactive_count vm.stats.vm.v_cache_count vm.stats.vm.v_free_count', file_get_contents(__DIR__ . '/../data/freebsd_meminfo')],
 			]);
 
 		$memory = $this->os->getMemory();
@@ -72,7 +72,7 @@ class FreeBSDTest extends TestCase {
 				if ($command === '/usr/sbin/swapinfo -k') {
 					throw new \RuntimeException('No output for command: /usr/sbin/swapinfo');
 				}
-				if ($command === '/sbin/sysctl -n hw.physmem hw.pagesize vm.stats.vm.v_inactive_count vm.stats.vm.v_cache_count vm.stats.vm.v_free_count') {
+				if ($command === '/sbin/sysctl -n hw.realmem hw.pagesize vm.stats.vm.v_inactive_count vm.stats.vm.v_cache_count vm.stats.vm.v_free_count') {
 					return file_get_contents(__DIR__ . '/../data/freebsd_meminfo');
 				}
 			});
