d529055376cf47d591b115b81cc76cecb7e8a737
szaimen <szaimen@e.mail.de>
2021-05-02 10:10:47 +0200

    Hide squashfs and overlay-FS from the overview
    Signed-off-by: szaimen <szaimen@e.mail.de>

--- lib/OperatingSystems/DefaultOs.php
+++ lib/OperatingSystems/DefaultOs.php
@@ -196,7 +196,7 @@ class DefaultOs implements IOperatingSystem {
 		}
 
 		foreach ($matches['Filesystem'] as $i => $filesystem) {
-			if (in_array($matches['Type'][$i], ['tmpfs', 'devtmpfs'], false)) {
+			if (in_array($matches['Type'][$i], ['tmpfs', 'devtmpfs', 'squashfs', 'overlay'], false)) {
 				continue;
 			}
 
