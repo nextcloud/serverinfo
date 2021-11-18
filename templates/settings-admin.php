3084c2f391f98e80707122f5f847d7d362fc6040
Valdnet <47037905+Valdnet@users.noreply.github.com>
2021-11-18 09:49:02 +0100

    Concatenate a colon with a text string
    Signed-off-by: Valdnet <47037905+Valdnet@users.noreply.github.com>

--- templates/settings-admin.php
+++ templates/settings-admin.php
@@ -56,18 +56,18 @@ $disks = $_['diskinfo'];
 				</h2>
 			</div>
 			<div class="col col-12">			
-				<p><?php p($l->t('Operating System').':'); ?> <strong id="numFilesStorage"><?php p($_['osname']); ?></strong></p>
-				<p><?php p($l->t('CPU').':'); ?>
+				<p><?php p($l->t('Operating System:')); ?> <strong id="numFilesStorage"><?php p($_['osname']); ?></strong></p>
+				<p><?php p($l->t('CPU:')); ?>
 				<?php if ($_['cpu'] !== 'Unknown Processor'): ?>
 				<strong id="numFilesStorage"><?php p($_['cpu']) ?></strong></p>
 				<?php else: ?>
 				<strong id="numFilesStorage"><?php p($l->t('Unknown Processor')) ?></strong></p>
 				<?php endif; ?>
-				<p><?php p($l->t('Memory').':'); ?>
+				<p><?php p($l->t('Memory:')); ?>
 				<?php if ($memory->getMemTotal() > 0): ?> <strong id="numFilesStorage"><?php p(FormatMegabytes($memory->getMemTotal())) ?></strong></p>
 				<?php endif; ?>
-				<p><?php p($l->t('Server time').':'); ?> <strong id="numFilesStorage"><span class="info" id="servertime"></span></strong></p>
-				<p><?php p($l->t('Uptime').':'); ?> <strong id="numFilesStorage"><span class="info" id="uptime"></span></strong></p>
+				<p><?php p($l->t('Server time:')); ?> <strong id="numFilesStorage"><span class="info" id="servertime"></span></strong></p>
+				<p><?php p($l->t('Uptime:')); ?> <strong id="numFilesStorage"><span class="info" id="uptime"></span></strong></p>
 			</div>
 		</div>
 	</div>
@@ -142,10 +142,10 @@ $disks = $_['diskinfo'];
 			<?php p($l->t('You will get a notification once one of your disks is nearly full.')); ?>
 		</div>
 
-		<p><?php p($l->t('Files').':'); ?> <strong id="numFilesStorage"><?php p($_['storage']['num_files']); ?></strong></p>
-		<p><?php p($l->t('Storages').':'); ?> <strong id="numFilesStorages"><?php p($_['storage']['num_storages']); ?></strong></p>
+		<p><?php p($l->t('Files:')); ?> <strong id="numFilesStorage"><?php p($_['storage']['num_files']); ?></strong></p>
+		<p><?php p($l->t('Storages:')); ?> <strong id="numFilesStorages"><?php p($_['storage']['num_storages']); ?></strong></p>
 		<?php if ($_['system']['freespace'] !== null): ?>
-			<p><?php p($l->t('Free Space').':'); ?> <strong id="systemDiskFreeSpace"><?php p($_['system']['freespace']); ?></strong></p>
+			<p><?php p($l->t('Free Space:')); ?> <strong id="systemDiskFreeSpace"><?php p($_['system']['freespace']); ?></strong></p>
 		<?php endif; ?>
 	</div>
 
@@ -224,7 +224,7 @@ $disks = $_['diskinfo'];
 							></canvas>
 						</div>
 						<p>
-							<?php p($l->t('Total users').':'); ?>
+							<?php p($l->t('Total users:')); ?>
 							<em id="numUsersStorage"><?php p($_['storage']['num_users']); ?></em>
 						</p>
 					</div>
@@ -266,19 +266,19 @@ $disks = $_['diskinfo'];
 				<div class="infobox">
 					<div class="phpinfo-wrapper">
 						<p>
-							<?php p($l->t('Version').':'); ?>
+							<?php p($l->t('Version:')); ?>
 							<em id="phpVersion"><?php p($_['php']['version']); ?></em>
 						</p>
 						<p>
-							<?php p($l->t('Memory limit').':'); ?>
+							<?php p($l->t('Memory limit:')); ?>
 							<em id="phpMemLimit"><?php p($_['php']['memory_limit']); ?></em>
 						</p>
 						<p>
-							<?php p($l->t('Max execution time').':'); ?>
+							<?php p($l->t('Max execution time:')); ?>
 							<em id="phpMaxExecTime"><?php p($_['php']['max_execution_time']); ?></em>
 						</p>
 						<p>
-							<?php p($l->t('Upload max size').':'); ?>
+							<?php p($l->t('Upload max size:')); ?>
 							<em id="phpUploadMaxSize"><?php p($_['php']['upload_max_filesize']); ?></em>
 						</p>
 					</div>
@@ -294,15 +294,15 @@ $disks = $_['diskinfo'];
 				<div class="infobox">
 					<div class="database-wrapper">
 						<p>
-							<?php p($l->t('Type').':'); ?>
+							<?php p($l->t('Type:')); ?>
 							<em id="databaseType"><?php p($_['database']['type']); ?></em>
 						</p>
 						<p>
-							<?php p($l->t('Version').':'); ?>
+							<?php p($l->t('Version:')); ?>
 							<em id="databaseVersion"><?php p($_['database']['version']); ?></em>
 						</p>
 						<p>
-							<?php p($l->t('Size').':'); ?>
+							<?php p($l->t('Size:')); ?>
 							<em id="databaseSize"><?php p($_['database']['size']); ?></em>
 						</p>
 					</div>
@@ -318,7 +318,7 @@ $disks = $_['diskinfo'];
 				<!-- OCS ENDPOINT -->
 				<h2><?php p($l->t('External monitoring tool')); ?></h2>
 				<p>
-					<?php p($l->t('You can connect an external monitoring tool by using this end point').':'); ?>
+					<?php p($l->t('You can connect an external monitoring tool by using this end point:')); ?>
 				</p>
 				<div class="monitoring-wrapper">
 					<input type="text" readonly="readonly" id="monitoring-endpoint-url" value="<?php echo p($_['ocs']); ?>"/>
@@ -328,7 +328,7 @@ $disks = $_['diskinfo'];
 					<?php p($l->t('Appending "?format=json" at the end of the URL gives you the result in JSON.')); ?>
 				</p>
 				<p>
-					<?php p($l->t('To use an access token please generate one then set it using the following command').':'); ?>
+					<?php p($l->t('To use an access token please generate one then set it using the following command:')); ?>
 					<div><i>occ config:app:set serverinfo token --value yourtoken</i></div>
 				</p>
 				<p>
