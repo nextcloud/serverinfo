1d8c6352332b2f434d6e03e6a2cb3eb5832c12ce
Grégory Marigot <gmarigot@teicee.com>
2024-03-18 11:20:10 +0100

    style: Compliance for the linter
    Signed-off-by: Grégory Marigot <gmarigot@teicee.com>

--- lib/Os.php
+++ lib/Os.php
@@ -74,17 +74,23 @@ class Os implements IOperatingSystem {
 	public function getDiskInfo(): array {
 		$disks = $this->backend->getDiskInfo();
 		$filters = $this->config->getSystemValue('serverinfo_disk_filter_paths', null);
-		if ($filters === null) return $disks;
-
+		if ($filters === null) {
+			return $disks;
+		}
 		// apply defined filters to restrict the list of disks according to their mount point
 		$filtered_disks = [];
 		foreach (explode(':', $filters) as $filter) {
 			// convert special filters to their corresponding paths
 			switch ($filter) {
-				case 'DOCROOT' : $path = isset($_SERVER['SCRIPT_FILENAME']) ? dirname($_SERVER['SCRIPT_FILENAME']) : $_SERVER['DOCUMENT_ROOT']; break;
-				case 'DATADIR' : $path = $this->config->getSystemValue('datadirectory', ''); break;
-				case 'TEMPDIR' : $path = $this->config->getSystemValue('tempdirectory', ''); break;
-				case 'LOGSDIR' : $path = dirname($this->config->getSystemValue('logfile', '')); break;
+				case 'DOCROOT' : $path = isset($_SERVER['SCRIPT_FILENAME']) ? dirname($_SERVER['SCRIPT_FILENAME']) : (isset(
+$_SERVER['DOCUMENT_ROOT']) ? $_SERVER['DOCUMENT_ROOT'] : '');
+					break;
+				case 'DATADIR' : $path = $this->config->getSystemValue('datadirectory', '');
+					break;
+				case 'TEMPDIR' : $path = $this->config->getSystemValue('tempdirectory', '');
+					break;
+				case 'LOGSDIR' : $path = dirname($this->config->getSystemValue('logfile', ''));
+					break;
 				default : $path = $filter;
 			}
 			// find the disk whose mount point contains the filtering path
@@ -92,12 +98,14 @@ class Os implements IOperatingSystem {
 			$find_mount = '';
 			foreach ($disks as $index => $disk) {
 				$mount = $disk->getMount();
-				if (strncmp($path, $mount, strlen($mount)) !== 0) continue;
-				if (strlen($mount) < strlen($find_mount)) continue;
-				$find_mount = $mount;
-				$find_index = $index;
+				if ((strncmp($path, $mount, strlen($mount)) === 0) && (strlen($mount) >= strlen($find_mount))) {
+					$find_mount = $mount;
+					$find_index = $index;
+				}
 			}
-			if ($find_index !== null) $filtered_disks[ $find_index ] = $disks[ $find_index ];
+			if ($find_index !== null) {
+				$filtered_disks[ $find_index ] = $disks[ $find_index ];
+			}
 		}
 		return $filtered_disks;
 	}
