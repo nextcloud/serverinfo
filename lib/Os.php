54e9dd266956152c848aa08d29055ac95addf6ea
Daniel Kesselberg <mail@danielkesselberg.de>
2026-06-03 10:11:31 +0200

    style: Apply coding standard 1.5.0
    Signed-off-by: Daniel Kesselberg <mail@danielkesselberg.de>

--- lib/Os.php
+++ lib/Os.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
  * SPDX-FileCopyrightText: 2019 Nextcloud GmbH and Nextcloud contributors
  * SPDX-License-Identifier: AGPL-3.0-only
  */
+
 namespace OCA\ServerInfo;
 
 use OCA\ServerInfo\OperatingSystems\Dummy;
